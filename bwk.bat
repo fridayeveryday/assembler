2ca6c75d5b921a9531b7b984df539b8c3c380920
pavel a <p7ll7p@gmail.com>
2021-03-19 13:32:21 +0400

    second commit

---
+++ bwk.bat
@@ -0,0 +1,21 @@
+doskey
+cls
+echo off
+..\\tasm.exe mlab1.asm
+
+if errorlevel 1 goto edit
+
+..\\tasm.exe  mlab1l.asm
+
+if errorlevel 1 goto edit
+
+..\\tlink.exe mlab1.obj + mlab1l.obj
+
+if errorlevel 1 goto edit
+
+goto end
+
+:edit 
+	td.exe mlab1.asm
+:end	
+	mlab1.exe
