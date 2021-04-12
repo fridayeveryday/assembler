2505d5e59678ac1c1ef92ca740b247887012d4f7
pl <p7ll7p@gmail.com>
2021-04-12 09:47:32 +0400

    func for print dec number

--- mlab1.asm
+++ mlab1.asm
@@ -1,5 +1,5 @@
 ;***************************************************************************************************
-; MLAB1.ASM - учебный пример для выполнения 
+; MLAB1.ASM - учебный пример для выполнения
 ; лабораторной работы N1 по машинно-ориентированному программированию
 ; 10.09.02: Негода В.Н.
 ;***************************************************************************************************
@@ -7,11 +7,11 @@
         .STACK 200h
 	.386
 ;       Используются декларации констант и макросов
-        INCLUDE MLAB1.INC	
+        INCLUDE MLAB1.INC
         INCLUDE MLAB1.MAC
 
 ; Декларации данных
-        .DATA    
+        .DATA
 SLINE	DB	78 DUP (CHSEP), 0
 REQ	DB	"Фамилия И.О.: ",0FFh
 MINIS	DB	"МИНИСТЕРСТВО ОБРАЗОВАНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ",0
@@ -19,7 +19,7 @@ ULSTU	DB	"УЛЬЯНОВСКИЙ ГОСУДАРСТВЕННЫЙ ТЕХНИЧЕСКИЙ УНИВЕРСИТЕТ",0
 DEPT	DB	"Кафедра вычислительной техники",0
 MOP	DB	"Машинно-ориентированное программирование",0
 LABR	DB	"Лабораторная работа N 1",0
-REQ1    DB      "Замедлить(-),ускорить(+),выйти(ESC)? ",0FFh
+REQ1    DB      "Замедлить(-),ускорить(+),выйти(ESC), FUNC(1)? ",0FFh
 TACTS   DB	"Время работы в тактах: ",0FFh
 EMPTYS	DB	0
 BUFLEN = 70
@@ -29,6 +29,14 @@ SNAME	DB	BUFLEN DUP (0)
 PAUSE	DW	0, 0 ; младшее и старшее слова задержки при выводе строки
 TI	DB	LENNUM+LENNUM/2 DUP(?), 0 ; строка вывода числа тактов
                                           ; запас для разделительных "`"
+
+MYBUF DB BUFLEN
+X DD 0
+Y DD 0
+Z DD 0
+D DB '---------------------------------'
+
+
 
 ;========================= Программа =========================
         .CODE
@@ -43,8 +51,16 @@ BEGIN	LABEL	NEAR
 	MOV	PAUSE+2,PAUSE_H
 	PUTLS	REQ	; запрос имени
 	; ввод имени
-	LEA	DX,	BUF
-	CALL	GETS	
+	LEA	DX,	MYBUF
+	CALL	GETS
+  XOR ECX,ECX
+  XOR EAX,EAX
+  MOV CL, MYBUF[1]
+  MOV AL, CL
+  ADD AL, 48
+  CALL PUTC
+
+
 @@L:	; циклический процесс повторения вывода заставки
 	; вывод заставки
 	; ИЗМЕРЕНИЕ ВРЕМЕНИ НАЧАТЬ ЗДЕСЬ
@@ -52,13 +68,13 @@ BEGIN	LABEL	NEAR
 	PUTL	EMPTYS
 	PUTL	SLINE	; разделительная черта
 	PUTL	EMPTYS
-	PUTLSC	MINIS	; первая 
+	PUTLSC	MINIS	; первая
 	PUTL	EMPTYS
-	PUTLSC	ULSTU	;  и  
+	PUTLSC	ULSTU	;  и
 	PUTL	EMPTYS
-	PUTLSC	DEPT	;   последующие 
+	PUTLSC	DEPT	;   последующие
 	PUTL	EMPTYS
-	PUTLSC	MOP	;    строки  
+	PUTLSC	MOP	;    строки
 	PUTL	EMPTYS
 	PUTLSC	LABR	;     заставки
 	PUTL	EMPTYS
@@ -67,11 +83,11 @@ BEGIN	LABEL	NEAR
 	PUTL	EMPTYS
 	; разделительная черта
 	PUTL	SLINE
-	; ИЗМЕРЕНИЕ ВРЕМЕНИ ЗАКОНЧИТЬ ЗДЕСЬ 
+	; ИЗМЕРЕНИЕ ВРЕМЕНИ ЗАКОНЧИТЬ ЗДЕСЬ
 	DURAT    	; подсчет затраченного времени
 	; Преобразование числа тиков в строку и вывод
 	LEA	DI,	TI
-	CALL	UTOA10	
+	CALL	UTOA10
 	PUTL	TACTS
 	PUTL	TI      ; вывод числа тактов
 	; обработка команды
@@ -83,18 +99,98 @@ BEGIN	LABEL	NEAR
 	JMP	@@L
 CMINUS:	CMP	AL,	'+'    ; укорачивать задержку?
 	JNE	CEXIT
-	CMP	WORD PTR PAUSE+2, 0		
+	CMP	WORD PTR PAUSE+2, 0
 	JE	BACK
 	DEC	PAUSE+2        ; убавить 65536 мкс
 BACK:	JMP	@@L
-CEXIT:	CMP	AL,	CHESC	
+CEXIT:	CMP	AL,	CHESC
 	JE	@@E
 	TEST	AL,	AL
+  CMP AL, '1'
+  JE MY_PROG
+  PUTLSC	SNAME   ; ФИО студента
 	JNE	BACK
 	CALL	GETCH
 	JMP	@@L
+
+
+  ; RESULT IN EAX , STRING WITH BINARY NUMBER IN MYBUFF
+  READ_NUM PROC NEAR
+    xor eax, eax
+    xor ebx, ebx
+    XOR EDX, EDX
+    MOV EDX, ESI
+    ADD EDX, 1
+    MOV CL, [EDX]
+    ADD ESI, 2
+    XOR ECX,ECX
+    l1:
+       mov bl, [esi]
+       sub bl, 30h
+       add al, bl
+       shl al, 1
+       inc esi
+       loop l1
+    shr al,1
+    PUTL	EMPTYS
+    PUTL BUF
+    PUTL	EMPTYS
+
+    LEA ESI, BUF
+    MOV ESI, EAX
+    PUTL BUF
+    ; CALL	PUTC
+    RET
+  READ_NUM ENDP
+
+  ; WRITE DEC NUMBER FROM EAX
+  PRINT_DEC PROC NEAR
+    MOV EBX, 10
+    XOR ECX, ECX
+    DIVISION:
+      XOR EDX, EDX
+      DIV EBX
+      ADD EDX, 48
+      push EDX
+      ADD ECX, 1
+      CMP EAX, 0
+      JG DIVISION
+    PRINT:
+      XOR EAX, EAX
+      POP EAX
+      CALL PUTC
+      LOOP PRINT
+      RET
+    PRINT_DEC ENDP
+
+
+MY_PROG:
+  ; PUTL	EMPTYS
+  ; LEA	DX,	MYBUF
+  ; CALL	GETS
+  ; XOR ECX,ECX
+  ; XOR EAX,EAX
+  ; MOV CL, MYBUF[1]
+  ; MOV AL, CL
+  ; ADD AL, 48
+  ; PUTL	EMPTYS
+  ; CALL PUTC
+  ; PUTL	EMPTYS
+  ; XOR ESI, esi
+  ; LEA ESI, [MYBUF]
+  ; CALL READ_NUM
+  PUTL	EMPTYS
+  XOR EAX, EAX
+  MOV EAX, 462342
+
+
+  CALL PRINT_DEC
+  JMP @@E
+
+
+
 	; Выход из программы
-@@E:	EXIT	
+@@E:	EXIT
         EXTRN	PUTSS:  NEAR
         EXTRN	PUTC:   NEAR
 	EXTRN   GETCH:  NEAR
